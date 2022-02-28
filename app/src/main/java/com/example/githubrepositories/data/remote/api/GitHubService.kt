1bad8774dfc9fbf6fc3f74dac4ca9a4971342d52
Marina Xavier de Oliveira <marinaxvier@gmail.com>
2022-02-28 12:41:01 -0300

    GitHubService response fix

--- app/src/main/java/com/example/githubrepositories/data/remote/api/GitHubService.kt
+++ app/src/main/java/com/example/githubrepositories/data/remote/api/GitHubService.kt
@@ -1,9 +1,10 @@
 package com.example.githubrepositories.data.remote.api
 
+import com.example.githubrepositories.model.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface GitHubService {
     @GET("?q=language:kotlin&sort=stars&page={page}")
-    suspend fun getRepositories(@Path("page") page: Int): Response<com.example.githubrepositories.model.Response>
+    suspend fun getRepositories(@Path("page") page: Int): Response
 }
